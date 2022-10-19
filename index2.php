4f2b9a3125f84ab9b9076f30fbeb5ba52072ac1e
Ramon Garrido <ramongarrido430@gmail.com>
2022-10-19 08:57:08 +0200

    index2.php

---
+++ index2.php
@@ -0,0 +1,31 @@
+<?php
+
+$nombre = $_GET['nombre'];
+$apellidos = $_GET['apellidos'];
+
+echo "Hola ".$nombre." ".$apellidos;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo</title>
+</head>
+<body>
+    <form action="index2.php" method="post">
+        <div>
+            <label>Introduzca su nombre</label>
+            <input type="text" name="nombre">
+        </div>
+        <div>
+            <label>Introduzca el numero que de veces que quiere que aparezca</label>
+            <input type="text" name="apellidos">
+            <input type="submit" value="Enviar"> 
+        </div>
+    </form>
+</body>
+</html>
